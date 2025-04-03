c86d8073b629e8b610411805da08a7e658d81a76
Suryansh Bhati <suryanshbhati20@gmail.com>
2025-04-03 12:11:29 +0530

    envv

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
